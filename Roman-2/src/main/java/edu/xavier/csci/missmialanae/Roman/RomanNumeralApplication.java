c68fa489d99b488f3bad79647b5074348d584b73
Mia <mialanae@outlook.com>
2019-04-03 23:09:08 -0400

    Second Commit

---
+++ Roman-2/src/main/java/edu/xavier/csci/missmialanae/Roman/RomanNumeralApplication.java
@@ -0,0 +1,13 @@
+package edu.xavier.csci.missmialanae.Roman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanNumeralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomanNumeralApplication.class, args);
+	}
+
+}
